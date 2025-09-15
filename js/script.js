0520571d7ddca596b4ede924b3221520d8fd93d6
Louis Nell <Louisnell@web.de>
2025-09-16 01:00:03 +0200

    Comment out dithering texture creation

--- js/script.js
+++ js/script.js
@@ -856,7 +856,7 @@ let bloomFramebuffers = [];
 let sunrays;
 let sunraysTemp;
 
-let ditheringTexture = createTextureAsync('LDR_LLL1_0.png');
+//let ditheringTexture = createTextureAsync('LDR_LLL1_0.png');
 
 const blurProgram            = new Program(blurVertexShader, blurShader);
 const copyProgram            = new Program(baseVertexShader, copyShader);
